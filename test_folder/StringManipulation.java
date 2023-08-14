f194e4656516a6c85af6829f438f83574c908665
David Giragori <davidantonygiragori@gmail.com>
2023-08-14 18:48:23 +0000

    Ok

---
+++ test_folder/StringManipulation.java
@@ -0,0 +1,11 @@
+public class StringManipulation {
+    public static void main (String [] args) {
+
+        String custName = "David G";
+        String itemDesc = "Watch";
+
+        String message = custName + " wants to purchase " + itemDesc + ".";
+
+        System.out.println (message);
+    }
+}
